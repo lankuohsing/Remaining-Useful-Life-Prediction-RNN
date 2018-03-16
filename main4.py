1a1e68f10b6ad5c93cd62c53e2e53de96f7238dd
lankuohsing <lankuohsing@163.com>
2018-03-17 00:01:29 +0800

    cancel test

--- main4.py
+++ main4.py
@@ -121,7 +121,7 @@ for FLAGS.num_layers in [1,2,3,4]:
                         )
                 if FLAGS.run_mode=="train":
                     rnn_model.train(RUL_Data, FLAGS)
-                    '''
+
                     final_test_pred_list=rnn_model.test(RUL_Data, FLAGS)
                     final_test_pred_last_np=np.array([final_test_pred_list[i][0][-1] for i in range(len(final_test_pred_list))])
                     a0=final_test_pred_last_np - final_test_RUL
@@ -130,7 +130,7 @@ for FLAGS.num_layers in [1,2,3,4]:
                     S=np.sum(b)
                     print("S:",S)
                     S_list.append(S)
-                    '''
+
                 else:
                     rnn_model.load()
                     final_test_pred_list=rnn_model.test(RUL_Data, FLAGS)
